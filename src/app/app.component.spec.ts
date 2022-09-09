61ccf5c43cd0f5b3bbea887f0789e1d68002a44d
Emil Juchnikowski <emil@juchnikowski.com>
2022-09-09 13:39:12 +0200

    Tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,23 +1,23 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { TestBed, waitForAsync } from '@angular/core/testing';
+import { RouterTestingModule} from '@angular/router/testing';
+import { Spectator, createComponentFactory } from '@ngneat/spectator';
 
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let spectator: Spectator<AppComponent>;
+  const createComponent = createComponentFactory({
+    component: AppComponent,
+    declarations: [AppComponent],
+    imports: [RouterTestingModule],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  });
 
-  beforeEach(waitForAsync(() => {
-
-    TestBed.configureTestingModule({
-      declarations: [AppComponent],
-      schemas: [CUSTOM_ELEMENTS_SCHEMA],
-    }).compileComponents();
-  }));
+  beforeEach(() => spectator = createComponent());
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
+    const app = spectator.component;
     expect(app).toBeTruthy();
   });
-  // TODO: add more tests!
 
 });
