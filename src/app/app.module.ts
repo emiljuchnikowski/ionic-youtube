7f7bbe324cf0bb7d21acfacc16a62ccd2287b282
Emil Juchnikowski <emil@juchnikowski.com>
2022-10-14 16:56:04 +0200

    SQLite

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 import { IonicStorageModule } from '@ionic/storage-angular';
+import { SQLite } from '@awesome-cordova-plugins/sqlite/ngx';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
@@ -27,7 +28,7 @@ import { Drivers } from '@ionic/storage';
       driverOrder: [ Drivers.LocalStorage, Drivers.IndexedDB ]
     })
   ],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, SQLite],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/services/storage.service.ts
+++ src/app/services/storage.service.ts
@@ -1,24 +1,56 @@
 import { Injectable } from "@angular/core";
-import { Storage } from '@ionic/storage-angular';
+import { SQLite, SQLiteObject } from '@awesome-cordova-plugins/sqlite/ngx';
 
 @Injectable({
     providedIn: "root"
 })
 export class StorageService {
-    private _storage: Storage | null = null;
+    private _storage: SQLiteObject | null = null;
 
-    constructor(private storage: Storage) { }
+    constructor(private sqlite: SQLite) { }
 
     async init(): Promise<void> {
-        const storage = await this.storage.create();
+        const storage = await this.sqlite.create({
+            name: 'testowa.db',
+            location: 'default'
+          });
         this._storage = storage;
+
+        await this._storage.executeSql(`create table if not exists test (id text primary key, body text)`,[]);
     }
 
     async get<T>(key: string): Promise<T> {
-        return await this._storage.get(key);
+        const sqlResult = await this._storage.executeSql(`select body from test where id = '${key}'`, []);
+        const result = sqlResult.rows.item(0);
+
+        if (!result) return null;
+
+        return JSON.parse(result.body);
     }
 
     async set<T>(key: string, value: T): Promise<void> {
-        await this._storage.set(key, value);
+        await this._storage.executeSql(`insert or replace into test (id, body) values ('${key}', '${JSON.stringify(value)}')`, []);
     }
 }
+
+// @Injectable({
+//     providedIn: "root"
+// })
+// export class StorageService {
+//     private _storage: Storage | null = null;
+
+//     constructor(private storage: Storage) { }
+
+//     async init(): Promise<void> {
+//         const storage = await this.storage.create();
+//         this._storage = storage;
+//     }
+
+//     async get<T>(key: string): Promise<T> {
+//         return await this._storage.get(key);
+//     }
+
+//     async set<T>(key: string, value: T): Promise<void> {
+//         await this._storage.set(key, value);
+//     }
+// }
